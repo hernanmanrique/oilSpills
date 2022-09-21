f6aa1a65e4d3a17332221ad9c824547679121a06
Hernán Manrique López <hernan.manrique@pucp.pe>
2022-09-21 11:57:56 +0200

    Final plot

--- Figure 1. Oil spills by cause.R
+++ Figure 1. Oil spills by cause.R
@@ -1,9 +1,6 @@
 #Load libraries
 library(tidyverse)
 library(readxl) 
-library(sf)
-library(ggsn)
-library(raster)
 library(grateful)
 (.packages())
 search()
@@ -12,19 +9,24 @@ objects(2) #el 1 siempre es para el global environment
 
 #Figure 1. Oil spills by cause#######################
 #Set wd, check files and import data
-setwd(r'(C:\Users\u0126759\Desktop\HM\Scholar shit\Papers\1. Ongoing\Oil spill and environmental justice con JC\2022\Julio & Setiembre\Data, graphs & maps)')
+setwd(r'(C:\Users\u0126759\Desktop\HM\Scholar shit\Papers\1. Ongoing\Oil spill and environmental justice con JC\2022\Setiembre (tras revisión)\version-control\oilSpills\data)')
 dir()
-causes <- read_excel("oxfam_long.xlsx")
+causes <- read_excel("Figure1-spillsByCause.xlsx")
 head(causes)
 
 #Plot causes
 causes %>% 
   ggplot(aes(x=Year, y=Spills, fill=Cause)) +
   geom_line(aes(linetype=Cause)) + #line type
-  ggtitle("Oil Spills by cause, 2000-2019") +
+  ggtitle("Oil spills by cause, 2000-2019") +
   scale_x_continuous(limits=c(2000, 2019), breaks = seq(2000, 2019, 2))+
-  theme(plot.title = element_text(size = 12, face = "bold"),
-        legend.position = "bottom") +
+  theme_bw() +
+  theme(plot.title = element_text(size = 11, face = "bold"),
+        legend.text=element_text(size=7),
+        legend.title=element_text(size=8),
+        legend.position = "bottom",
+        axis.title = element_text(size = 9)) +
   labs(y = "Number of oil spills") 
+  
 
-causes %>% group_by(Year) %>% summarize(Cause)
+
